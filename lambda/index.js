98361728002111b729d174fbe3665c6a959787ec
Priyansh Singh <thetseffect@gmail.com>
2020-08-22 16:55:08 +0530

    Added RPN

--- lambda/index.js
+++ lambda/index.js
@@ -2,16 +2,17 @@
 // Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
 // session persistence, api calls, and more.
 const Alexa = require("ask-sdk-core");
-const axios = require("axios");
 
-function calltheAPI() {
-  return axios
-    .get(
-      "https://api.spoonacular.com/recipes/findByNutrients?minCarbs=10&maxCarbs=50&number=1&apiKey=dce5c0d84f274548a3edb7a7b661c3de"
-    )
-    .then((response) => {
-      return response;
-    });
+const rp = require("request-promise-native");
+
+async function getData() {
+  var options = {
+    uri: `https://api.spoonacular.com/recipes/findByNutrients?minCarbs=10&maxCarbs=50&number=1&apiKey=dce5c0d84f274548a3edb7a7b661c3de`,
+    json: true,
+  };
+
+  var response = await rp(options);
+  return response;
 }
 
 const LaunchRequestHandler = {
@@ -56,22 +57,19 @@ const InputConstraintsIntentHandler = {
         "InputConstraintsIntent"
     );
   },
-  handle(handlerInput) {
-    // CALL THE API HERE AND RETURN THE RESULTS IN THE REPONSE
-    let speakOutput = `Searching...`;
-    // make a sample call
-    calltheAPI()
+  async handle(handlerInput) {
+    let speakOutput = "";
+    const data = getData();
+    await data
       .then((response) => {
         console.log(response.data);
         speakOutput = `I found ${response.data[0]["title"]} that will give you ${response.data[0]["calories"]} calories.`;
-         return handlerInput.responseBuilder.speak(speakOutput).getResponse();
       })
       .catch((err) => {
         console.log(err);
         speakOutput = `Sorry, I was not able to retrieve your meals. Please try again.`;
-         return handlerInput.responseBuilder.speak(speakOutput).getResponse();
       });
-   
+    return handlerInput.responseBuilder.speak(speakOutput).getResponse();
   },
 };
 
