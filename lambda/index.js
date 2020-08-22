9a6e10ef50fa21346a91f99e660faa98918ef590
Priyansh Singh <thetseffect@gmail.com>
2020-08-22 17:29:17 +0530

    Added RPN

--- lambda/index.js
+++ lambda/index.js
@@ -2,18 +2,18 @@
 // Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
 // session persistence, api calls, and more.
 const Alexa = require("ask-sdk-core");
+const axios = require("axios");
 
-const rp = require("request-promise-native");
-
-async function getData() {
-  var options = {
-    uri: `https://api.spoonacular.com/recipes/findByNutrients?minCarbs=10&maxCarbs=50&number=1&apiKey=dce5c0d84f274548a3edb7a7b661c3de`,
-    json: true,
-  };
-
-  var response = await rp(options);
-  return response;
-}
+const callAPI = async () => {
+  try {
+    const response = await axios.get(
+      `https://api.spoonacular.com/recipes/findByNutrients?minCarbs=10&maxCarbs=50&number=1&apiKey=dce5c0d84f274548a3edb7a7b661c3de`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
@@ -58,9 +58,8 @@ const InputConstraintsIntentHandler = {
     );
   },
   async handle(handlerInput) {
-    let speakOutput = "";
-    const data = getData();
-    await data
+    let speakOutput = ``;
+    const responseData = await callAPI()
       .then((response) => {
         console.log(response.data);
         speakOutput = `I found ${response.data[0]["title"]} that will give you ${response.data[0]["calories"]} calories.`;
--- lambda/testing.js
+++ lambda/testing.js
@@ -1,12 +1,45 @@
+const rp = require("request-promise-native");
 const axios = require("axios");
 
-axios
-  .get(
-    "https://api.spoonacular.com/recipes/findByNutrients?minCarbs=10&maxCarbs=50&number=2&apiKey=dce5c0d84f274548a3edb7a7b661c3de"
-  )
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((err) => {
-    console.log(err);
+// async function getData() {
+//   var options = {
+//     uri: `https://api.spoonacular.com/recipes/findByNutrients?minCarbs=10&maxCarbs=50&number=1&apiKey=dce5c0d84f274548a3edb7a7b661c3de`,
+//     json: true,
+//   };
+//   var response = await rp(options);
+//   console.log("here" + response);
+//   return response;
+// }
+
+// async function rundata() {
+//   const result = await getData();
+//   console.log(result.data);
+// }
+
+// rundata();
+
+const callapi = async () => {
+  try {
+    const response = await axios.get(
+      "https://api.spoonacular.com/recipes/findByNutrients?minCarbs=10&maxCarbs=50&number=1&apiKey=dce5c0d84f274548a3edb7a7b661c3de"
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const newfun = async () => {
+  console.log("FIRST");
+
+  const result = await callapi().then((response) => {
+    console.log(response);
+    return response;
   });
+
+  console.log("LAST");
+
+  return result;
+};
+
+console.log(newfun());
